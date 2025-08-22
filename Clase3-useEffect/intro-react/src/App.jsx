8a434566397420131b0487c2cf1d0e76ab6218c0
Pablo Nunga <pablonungaray@protonmail.com>
2025-08-22 00:27:10 -0600

    Clase 3 - 10 Junio 2025

---
+++ Clase3-useEffect/intro-react/src/App.jsx
@@ -0,0 +1,34 @@
+import './App.css'
+import Card from './Components/Card'
+import Counter from './Components/Counter'
+import FilterableList from './Components/FilterableList'
+
+const alt = "dog-image"
+
+function App() {
+
+  return (
+    <>
+      <h1>Este es mi primer componente:</h1>
+      <Card 
+        src={"https://images.pexels.com/photos/1108099/pexels-photo-1108099.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"} 
+        alt={alt} 
+        text={"Perro 1"}
+      />
+      <Card 
+        src={"https://images.pexels.com/photos/1108099/pexels-photo-1108099.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"} 
+        alt={alt}
+        text={"Perro 4"}
+      />
+      <Card 
+        src={"https://images.pexels.com/photos/1108099/pexels-photo-1108099.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"} 
+        alt={alt}
+        text={"Perro 3"}
+      />
+      <Counter />
+      <FilterableList />
+    </>
+  )
+}
+
+export default App
