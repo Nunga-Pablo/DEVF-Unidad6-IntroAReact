e67ecd6e9b6bf5deea62479455636d1d1b5652ee
Pablo Nunga <pablonungaray@protonmail.com>
2025-08-24 19:54:04 -0600

    Clase 5 - 17 junio 2025

---
+++ Clase5-conditionalRendering/intro-react/src/Components/Counter.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import '../css/Counter.css'
+
+export default function Counter({counter, setCounter}){
+
+    function handleSubstracButton(){
+        if(counter <= 0){
+            return;
+        }
+        setCounter(counter - 1)
+    }
+
+    function handleAddingButton(){
+        setCounter(counter + 1)
+    }
+
+
+    return(
+        <div className="counter-container">
+            <button onClick={() => {handleSubstracButton()}}>-</button>
+            <h1>{counter}</h1>
+            <button onClick={() => handleAddingButton()}>+</button>
+        </div>
+    )
+}
